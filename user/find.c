98483402aea3bb4df072fa0ba050c9949ffe3efb
SyedHasanImam <syedhasanimam.r@email.com>
2025-09-25 08:12:31 -0700

    firstAssignment: extend find to support regex

--- user/find.c
+++ user/find.c
@@ -4,6 +4,9 @@
 #include "user/user.h"
 #include "kernel/fcntl.h"
 #include "kernel/param.h"
+#include "kernel/param.h"
+
+int match(char *re, char *text);
 
 char*
 fmtname(char *path)
@@ -44,7 +47,7 @@ find(char *path, char *name, int doexec, char *cmd[])
   }
 
   if(st.type == T_FILE){
-    if(strcmp(fmtname(path), name) == 0){
+    if(match(name, fmtname(path))){
       if(doexec){
   int pid = fork();
   if(pid == 0){
@@ -112,3 +115,43 @@ main(int argc, char *argv[])
   exit(0);
 }
 
+
+
+
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+int
+match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do{  // must look at empty string
+    if(matchhere(re, text))
+      return 1;
+  }while(*text++ != '\0');
+  return 0;
+}
+// matchhere: search for re at beginning of text
+int matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+  do{  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
+}
